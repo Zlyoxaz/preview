9e3f5d5d68f697b91acaf6017ed7b816c8553ee8
Alexey <mr.alexey537@gmail.com>
2024-02-07 15:46:33 +0300

    1less

--- js/script.js
+++ js/script.js
@@ -1,73 +1,42 @@
-// const arr = [5,
-    
-//     5,
-    
-    
-//         5];
+/* Задание на урок:
 
-// function ()) {
+1) Создать переменную numberOfFilms и в неё поместить ответ от пользователя на вопрос:
+'Сколько фильмов вы уже посмотрели?'
 
-// }  
+2) Создать объект personalMovieDB и в него поместить такие свойства:
+    - count - сюда передается ответ на первый вопрос
+    - movies - в это свойство поместить пустой объект
+    - actors - тоже поместить пустой объект
+    - genres - сюда поместить пустой массив
+    - privat - в это свойство поместить boolean(логическое) значение false
+
+3) Задайте пользователю по два раза вопросы:
+    - 'Один из последних просмотренных фильмов?'
+    - 'На сколько оцените его?'
+Ответы стоит поместить в отдельные переменные
+Записать ответы в объект movies в формате: 
+    movies: {
+        'logan': '8.1'
+    }
+
+Проверить, чтобы все работало без ошибок в консоли */
+
 'use strict';
 
-// alert('Hello'); предупреждение
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+const personalMovieDB = {
+	count: numberOfFilms,
+	movies: {},
+	actors: {},
+	genres: [],
+	privat: false
+};
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+	  b = prompt('На сколько оцените его?', ''),
+	  c = prompt('Один из последних просмотренных фильмов?', ''),
+	  d = prompt('На сколько оцените его?', '');
 
-// const result = confirm('Are you here?');
-// console.log(result); согласие
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
 
-// const answer = prompt('Вам есть 18,','18');
-// console.log(answer); ответ от пользователя
-
-const answers = [];
-
-answers[0] = prompt('Как ваше имя?', '');
-answers[1] = prompt('Как ваше Фамилия?', '');
-answers[2] = prompt('Сколько вам лет?', '');
-
-console.log(typeof(answers));
-console.log(typeof(null));
-
-
-
-
-// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-
-const category = 'toys';
-
-console.log(`https://someurl.com/${category}/5`);
-
-
-const user = 'Alex';
-
-alert(`Привет, ${user}`);
-
-
-// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-console.log('arr' + ' - object');
-console.log(4 + +'5');
-
-let incr = 10,
-	decr = 10;
-
-// ++incr;
-// --decr;
-
-console.log(++incr);
-console.log(--decr);
-
-console.log(5%2);
-
-console.log(2 + 2 * 2 != 8); 
-// == - сравнение, === - строгое сравнение (включает сравнение типов данных)
-
-// && /* и */
-// || /* или */
-// ! - отрицание
-// != - не равен
-
-const isChecked = false,
-	isClose = false;
-
-console.log(isChecked || !isClose);
+console.log(personalMovieDB);
